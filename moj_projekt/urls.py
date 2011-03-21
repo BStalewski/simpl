57c7f8f020e76f78562ca9a370185e8bd971d8b6
Bartosz Stalewski <Bartek@.(none)>
2011-03-21 19:42:29 +0100

    urls

--- moj_projekt/urls.py
+++ moj_projekt/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls.defaults import *
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
+#
+#
 
 urlpatterns = patterns('',
     # Example:
